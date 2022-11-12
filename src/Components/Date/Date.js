1615182adade8740b4ee020159c1f0dfb74bf860
Adriano Silva <adrianowic0101@gmail.com>
2022-11-12 15:29:26 -0300

    comentando as informações no componente App

--- src/Components/Date/Date.js
+++ src/Components/Date/Date.js
@@ -22,7 +22,63 @@ import PokemonCard from "../PokemonCard/PokemonCard.js";
         })
     }
     
+    const [vulpix, setVulpix] = useState({
+        name: 'Vulpix',
+        type: 'Fire',
+        envolved: true,
+        weight: 9.9,
+        color: 'orange',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/037.png'
+    })
     
+    const evoluirVulpix = () => {
+        setVulpix({
+        name: 'Ninetales',
+        type: 'Fire',
+        envolved: true,
+        weight: 19.9,
+        color: 'red',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/038.png'
+        }) 
+    }
+    
+    const [bulbasaur, setBulbasaur] = useState({
+      name: 'Bulbasaur',
+      type: 'Grass & Poison',
+      envolved: true,
+      weight: 6.9,
+      color: 'MediumSpringGreen',
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+    })
+    const evoluirBulbasaur = () => {
+      setBulbasaur({
+        ...bulbasaur,
+        name: 'Ivysaur',
+        weight: 13.00,
+        color: 'MediumSeaGreen',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png'
+        
+      })
+    }
+    
+    const [squirtle, setSquirtle] = useState({
+      name: 'Squirtle',
+      type: 'Water',
+      envolved: true,
+      weight: 9,
+      color: '#8EC3D0',
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
+    })
+    const evoluirSquirtle = () => {
+      setSquirtle({
+        ...squirtle,
+      name: 'Wartortle',
+      weight: 22.5,
+      color: '#6D8FBE',
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png'
+      })
+      
+    }
 
     <PokemonCard pokemon={pichu} evolution={evoluirPichu }/>
 
